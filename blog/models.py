f052ca6e08dd44d4be7eb5ce98ac3c77951d7d56
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-14 01:52:19 +0300

    Add files via upload

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)  # could later link to User
+    content = models.TextField()
+    published = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
