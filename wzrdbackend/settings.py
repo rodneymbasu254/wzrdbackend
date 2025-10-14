a6bdcf34b018ee8128a65ec9997905e4235731d7
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-14 20:43:00 +0300

    Update settings.py

--- wzrdbackend/settings.py
+++ wzrdbackend/settings.py
@@ -138,7 +138,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 #STATICFILES_DIRS = [
     #os.path.join(BASE_DIR, 'static'),
 #]
@@ -146,10 +148,12 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
 
 
 
