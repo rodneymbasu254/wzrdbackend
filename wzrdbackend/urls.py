5d2089fe83c68d6b1bd34d66ecc6c2d555256c18
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-15 12:56:12 +0300

    Update urls.py

--- wzrdbackend/urls.py
+++ wzrdbackend/urls.py
@@ -17,9 +17,6 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from django.conf import settings
-from django.conf.urls.static import static
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('projects.urls')),
@@ -30,10 +27,5 @@ urlpatterns = [
 
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-else:
-    from django.views.static import serve
-    from django.urls import re_path
 
-    urlpatterns += [
-        re_path(r'^media/(?P<path>.*)$', serve, {'document_root': settings.MEDIA_ROOT}),
-    ]
+
