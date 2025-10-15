762c65533af1d9cda8a3b712e3531fec5e699c27
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-15 09:45:25 +0300

    Update settings.py

--- wzrdbackend/settings.py
+++ wzrdbackend/settings.py
@@ -92,8 +92,6 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'wzrdbackend.wsgi.application'
-
-
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
@@ -149,6 +147,26 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
+
+# -------------------------------
+# AWS S3 SETTINGS
+# -------------------------------
+AWS_ACCESS_KEY_ID = os.getenv('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.getenv('AWS_SECRET_ACCESS_KEY')
+AWS_STORAGE_BUCKET_NAME = os.getenv('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.getenv('AWS_S3_REGION_NAME', 'us-east-1')
+AWS_QUERYSTRING_AUTH = False  # Optional: so URLs don’t expire
+
+# Optional custom domain (for cleaner URLs)
+AWS_S3_CUSTOM_DOMAIN = f"{AWS_STORAGE_BUCKET_NAME}.s3.amazonaws.com"
+
+# Default file storage (for uploaded media)
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# Optional: static files can also go to S3 (if you want)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+MEDIA_URL = f"https://{AWS_S3_CUSTOM_DOMAIN}/"
 
 
 
