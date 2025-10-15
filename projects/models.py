70cc53c38d761d038cc9c2c7eb48da8140f59995
Rodney Mbasu <rodneymbasu@gmail.com>
2025-10-15 12:31:34 +0300

    Update models.py

--- projects/models.py
+++ projects/models.py
@@ -1,6 +1,13 @@
 from django.db import models
+from imagekitio import ImageKit
+import os
 
-# Create your models here.
+imagekit = ImageKit(
+    private_key=os.getenv('IMAGEKIT_PRIVATE_KEY'),
+    public_key=os.getenv('IMAGEKIT_PUBLIC_KEY'),
+    url_endpoint=os.getenv('IMAGEKIT_URL_ENDPOINT')
+)
+
 class Project(models.Model):
     title = models.CharField(max_length=200)
     division_name = models.CharField(max_length=200, blank=True, null=True)
@@ -18,6 +25,16 @@ class Project(models.Model):
     )
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def save(self, *args, **kwargs):
+        if self.image and not self.image.url.startswith('https://ik.imagekit.io/'):
+            upload = imagekit.upload_file(
+                file=self.image,  # can be a file object
+                file_name=self.image.name,
+                folder="/projects/"
+            )
+            self.image = upload.url
+        super().save(*args, **kwargs)
 
     def __str__(self):
         return self.title
